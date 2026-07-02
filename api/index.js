7bec73d9bff79b1437c95ad20b6b642c09910385
suyash112006 <suyashzope789@gmail.com>
2026-07-02 23:02:50 +0530

    Fix Vercel serverless routing to explicitly target api/index.js

--- api/index.js
+++ api/index.js
@@ -8,7 +8,10 @@ const connectToDatabase = async () => {
     return;
   }
   try {
-    const db = await mongoose.connect(process.env.MONGO_URI);
+    const db = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     isConnected = db.connections[0].readyState;
     console.log('MongoDB connected');
   } catch (err) {
